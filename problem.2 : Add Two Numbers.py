dcb8b4ea6da17729212fb442e48084c9a7eaba06
Ahmed A.Abu-zeid <56373837+A-DME@users.noreply.github.com>
2023-01-24 14:14:02 +0200

    Medium

--- problem.2 : Add Two Numbers.py
+++ problem.2 : Add Two Numbers.py
@@ -5,7 +5,6 @@ Add the two numbers and return the sum as a linked list.
 You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 
 example...
-
 Input: l1 = [2,4,3], l2 = [5,6,4].........(l1: 2-->4-->3, l2: 5-->6-->4)
 Output: [7,0,8]
 Explanation: 342 + 465 = 807.
