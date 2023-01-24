063eab5c561a30d05e0fcfe7c0bee76b5a5c2e05
Ahmed A.Abu-zeid <56373837+A-DME@users.noreply.github.com>
2023-01-24 22:03:29 +0200

    Medium

--- problem.5 : Longest Palindromic Substring.py
+++ problem.5 : Longest Palindromic Substring.py
@@ -15,3 +15,14 @@ class Solution:
                 if sub == sub[::-1] and len(sub) > len(longest):
                     longest = sub
         return longest if longest else s[0] # as s maybe contains no palindromic substrings / very few chars
+# # Another Solution
+# class Solution:
+#     def longestPalindrome(self, s: str) -> str:
+#         longest=''
+#         for i in range(len(s)):
+#             for j in range(len(s),i,-1):
+#                 sub= s[i:j]
+#                 if len(longest) >= j-i: break
+#                 if sub == sub[::-1] and len(sub) > len(longest):
+#                     longest = sub
+#         return longest if longest else s[0]
